6d69a48da0657934b0d72bb0608b2b62b3ee929f
Sausoon Abedian <sausoon.abedian@arkos.am>
2025-08-23 08:31:33 -0700

    fix: added request parameter to the blog view functions

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def starting_page():
+def starting_page(request):
     pass
 
 
-def posts():
+def posts(request):
     pass
 
 
-def post_details():
+def post_details(request):
     pass
