11e71a2fdacd8724090656b5adb7cfd8bc38fe8e
Sausoon Abedian <sausoon.abedian@arkos.am>
2025-08-23 08:18:52 -0700

    fix: fixed a typo in the app url.py file

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlspatterns = [
+urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="post-page"),
     path("posts/<slug:slug>", views.post_details, name="post-detail-page")
