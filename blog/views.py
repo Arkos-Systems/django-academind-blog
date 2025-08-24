5839e9e536663064b1a631d5f7f0d065e6559eab
Sausoon Abedian <sausoon.abedian@arkos.am>
2025-08-23 17:27:56 -0700

    fix: correction on dummy data

--- blog/views.py
+++ blog/views.py
@@ -5,15 +5,14 @@ all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
-        "author": "Maximilian",
+        "author": "Abedian",
         "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
         "excerpt": """
             There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for 
             what happened whilst I was enjoying the view!
         """,
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
@@ -29,15 +28,14 @@ all_posts = [
     {
         "slug": "programming-is-fun",
         "image": "coding.jpg",
-        "author": "Maximilian",
+        "author": "Abedian",
         "date": date(2022, 3, 10),
         "title": "Programming Is Great!",
         "excerpt": """
             Did you ever spend hours searching that one error in your code? Yep - that's what happened to me 
             yesterday...
         """,
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
@@ -53,12 +51,11 @@ all_posts = [
     {
         "slug": "into-the-woods",
         "image": "woods.jpg",
-        "author": "Maximilian",
+        "author": "Abedian",
         "date": date(2020, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
